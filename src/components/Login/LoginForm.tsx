0f7fe2b621257be63346fcd393ab2cb8d659877f
dobroeytro <warm.citrus@gmail.com>
2024-06-02 20:40:22 +0300

    feat: добавил модалку при успешном логине

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, TextField } from "@mui/material";
 import { Formik, Form, Field } from "formik";
 import * as yup from "yup";
+import ModalSuccess from "../Modal/ModalSuccess";
 
 const validationSchema = yup.object({
   email: yup
@@ -12,53 +13,65 @@ const validationSchema = yup.object({
 });
 
 const LoginForm: React.FC = () => {
+  const [isLoginSuccessModalOpen, setIsLoginSuccessModalOpen] = useState(false);
+
   return (
-    <Formik
-      initialValues={{
-        email: "",
-        password: "",
-      }}
-      validationSchema={validationSchema}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      {({ errors, touched }) => (
-        <Form className="login__form form" action="/submit" method="post">
-          <Field
-            as={TextField}
-            className="form__input"
-            id="email"
-            name="email"
-            label="Почта"
-            variant="outlined"
-            type="email"
-            error={touched.email && Boolean(errors.email)}
-            helperText={touched.email && errors.email}
-          />
-          <Field
-            as={TextField}
-            className="form__input"
-            id="password"
-            name="password"
-            label="Пароль"
-            variant="outlined"
-            type="password"
-            error={touched.password && Boolean(errors.password)}
-            helperText={touched.password && errors.password}
-          />
-          <Button
-            className="form__button button"
-            type="submit"
-            variant="outlined"
-            color="primary"
-            size="medium"
-          >
-            Войти
-          </Button>
-        </Form>
-      )}
-    </Formik>
+    <>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={validationSchema}
+        onSubmit={(values, actions) => {
+          console.log(values);
+          setIsLoginSuccessModalOpen(true);
+          actions.resetForm();
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form className="login__form form" action="/submit" method="post">
+            <Field
+              as={TextField}
+              className="form__input"
+              id="email"
+              name="email"
+              label="Почта"
+              variant="outlined"
+              type="email"
+              error={touched.email && Boolean(errors.email)}
+              helperText={touched.email && errors.email}
+            />
+            <Field
+              as={TextField}
+              className="form__input"
+              id="password"
+              name="password"
+              label="Пароль"
+              variant="outlined"
+              type="password"
+              error={touched.password && Boolean(errors.password)}
+              helperText={touched.password && errors.password}
+            />
+            <Button
+              className="form__button button"
+              type="submit"
+              variant="outlined"
+              color="primary"
+              size="medium"
+            >
+              Войти
+            </Button>
+          </Form>
+        )}
+      </Formik>
+      <ModalSuccess
+        isOpen={isLoginSuccessModalOpen}
+        onClose={() => setIsLoginSuccessModalOpen(false)}
+        title="Успешный вход!"
+        info="С Возвращением!"
+      />
+    </>
   );
 };
 
