ec7927a177b36e91c93dbc8376478f4422c1e72f
dobroeytro <warm.citrus@gmail.com>
2024-06-05 13:47:10 +0300

    fix: поправил стилизацию псевдосостояний календаря

--- src/components/Calendar/MonthList.tsx
+++ src/components/Calendar/MonthList.tsx
@@ -2,39 +2,39 @@ import React from "react";
 import { MONTHS } from "../../utils/variables";
 
 interface MonthListProps {
-    currentMonth: number;
-    onSelectMonth: (month: number) => void;
-    tasksByMonth: { [key: number]: boolean }; // Ключ - номер месяца (0-11), значение - есть ли задачи в месяце
-  }
+  currentMonth: number;
+  onSelectMonth: (month: number) => void;
+  tasksByMonth: { [key: number]: boolean }; // Ключ - номер месяца (0-11), значение - есть ли задачи в месяце
+}
 
-  const MonthList: React.FC<MonthListProps> = ({
-    currentMonth,
-    onSelectMonth,
-    tasksByMonth,
-  }) => {
-    return (
-      <div className="calendar__months months">
-        {MONTHS.map((month, index) => (
-          <label key={index} className="months__label">
-            <input
-              type="radio"
-              name="month"
-              value={index}
-              checked={currentMonth === index}
-              onChange={() => onSelectMonth(index)}
-              className="months__radio"
-            />
-            <span
-              className={`months__name ${
-                currentMonth === index ? "months__name--active" : ""
-              } ${tasksByMonth[index] ? "months__name--has-tasks" : ""}`}
-            >
-              {month}
-            </span>
-          </label>
-        ))}
-      </div>
-    );
-  };
+const MonthList: React.FC<MonthListProps> = ({
+  currentMonth,
+  onSelectMonth,
+  tasksByMonth,
+}) => {
+  return (
+    <div className="calendar__months months">
+      {MONTHS.map((month, index) => (
+        <label key={index} className="months__label" tabIndex={0}>
+          <input
+            type="radio"
+            name="month"
+            value={index}
+            checked={currentMonth === index}
+            onChange={() => onSelectMonth(index)}
+            className="months__radio"
+          />
+          <span
+            className={`months__name ${
+              currentMonth === index ? "months__name--active" : ""
+            } ${tasksByMonth[index] ? "months__name--has-tasks" : ""}`}
+          >
+            {month}
+          </span>
+        </label>
+      ))}
+    </div>
+  );
+};
 
 export default MonthList;
