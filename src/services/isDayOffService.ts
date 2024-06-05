37289db11ad40bc9ef25df92764074d5aa45ba82
dobroeytro <warm.citrus@gmail.com>
2024-06-05 16:40:55 +0300

    fix: стили

--- src/services/isDayOffService.ts
+++ src/services/isDayOffService.ts
@@ -1,25 +1,3 @@
-// const API_URL = "https://isdayoff.ru/";
-
-// export const isDayOff = async (year: number, month: number): Promise<{ [key: number]: boolean }> => {
-//   const holidays: { [key: number]: boolean } = {};
-
-//   for (let day = 1; day <= new Date(year, month + 1, 0).getDate(); day++) {
-//     const formattedMonth = String(month + 1).padStart(2, '0');
-//     const formattedDay = String(day).padStart(2, '0');
-//     const response = await fetch(`${API_URL}${year}${formattedMonth}${formattedDay}`);
-
-//     if (response.ok) {
-//       const data = await response.text(); // API возвращает текст
-//       holidays[day] = data === '1'; // '1' означает выходной
-//       console.log('ok')
-//     } else {
-//       console.error(`Failed to fetch data for ${year}-${formattedMonth}-${formattedDay}`);
-//     }
-//   }
-
-//   return holidays;
-// };
-
 import pLimit from 'p-limit';
 
 const API_URL = "https://isdayoff.ru/";
