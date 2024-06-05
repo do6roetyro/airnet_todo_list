0af3ac9e61058988fcbb34b0857297cde214ba88
dobroeytro <warm.citrus@gmail.com>
2024-06-05 21:42:30 +0300

    fix: svg

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import logo from "../../assets/images/git.svg"
+
 
 const Footer: React.FC = () => {
   return (
-    <header className="main-footer">
-      <div className="main-footer__wrapper wrapper">ссылка на гит</div>
-    </header>
+    <footer className="main-footer">
+      <div className="main-footer__wrapper wrapper">
+        <a
+          className="main-footer__link main-footer__link--github link"
+          href="https://github.com/do6roetyro"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="visually-hidden">Гитхаб</span>
+          <img
+            src={logo as unknown as string}
+            alt="Гитхаб"
+            className="social__logo"
+            width={50}
+            height={50}
+          />
+        </a>
+      </div>
+    </footer>
   );
 };
 
